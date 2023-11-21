3daacd1857d50af4bbe13c0c979ac71851cd4610
Michael <Michael@DESKTOP-6395OH7>
2023-11-21 15:06:24 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Jumia_Sorts.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
